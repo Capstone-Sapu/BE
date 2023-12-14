9a2141d64f8d61bce9e3dadeb65f9ec405c3d0d9
Akhmad Sugiannoor <AkhmadSugiannoor9@gmail.com>
2023-12-14 20:37:56 +0800

    Menambahkan Firebase

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -2,6 +2,21 @@ import fs from 'fs';
 import Item from "../models/ItemModel.js";
 import path from "path";
 import { Op } from 'sequelize';
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject,} from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsep7MLviWXB7C-L4zt6XI5kEgzoNrQmY",
+  authDomain: "sapu-project-af7ea.firebaseapp.com",
+  projectId: "sapu-project-af7ea",
+  storageBucket: "sapu-project-af7ea.appspot.com",
+  messagingSenderId: "159292158231",
+  appId: "1:159292158231:web:f521459b29dbba0e184752",
+  measurementId: "G-SBSGVKV41Y"
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
 
 
 export const getItems = async (req, res) => {
@@ -41,96 +56,132 @@ export const getItemById = async (req, res)=>{
     }
 }
 
-export const saveItem = (req, res)=>{
-    if(req.files === null) return res.status(400).json({msg: "No file Uploaded"});
-    const name = req.body.title;
+export const saveItem = async (req, res) => {
+    if (req.files === null) return res.status(400).json({ msg: "No file uploaded" });
+  
     const file = req.files.file;
-    const fileSize = file.data.length;
     const ext = path.extname(file.name);
     const fileName = file.md5 + ext;
-    const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
-    const description = req.body.description;
-    const price = req.body.price;
-    const allowedType = ['.png', '.jpg', '.jpeg'];
+    const storageRef = ref(storage, `images/${fileName}`);
+  
+    try {
+      await uploadBytes(storageRef, file.data);
+  
+      
+      const imageUrl = await getDownloadURL(storageRef);
+  
 
-    if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: 
-        "Invalid Images"});
-    if(fileSize > 5000000) return res.status(422).json({msg: "Image must be less than 5 MB"});
+      const name = req.body.title;
+      const description = req.body.description;
+      const price = req.body.price;
+  
+  
+      await Item.create({ name, image: fileName, url: imageUrl, description, price });
+  
+      res.status(201).json({ msg: "Item created successfully" });
+    } catch (error) {
+      console.error("Error uploading file to Firebase Cloud Storage:", error);
+      return res.status(500).json({ msg: error.message });
+    }
+  };
 
-    file.mv(`./public/images/${fileName}`, async(err)=>{
-        if(err) return res.status(500).json({msg: err.message});
-        try {
-            await Item.create({name: name, image: fileName, url: url, description: description, price: price});
-            res.status(201).json({msg: "Item Created Successfuly"});
-        } catch(error) {
-            console.log(error.message);
-        }
-    })
-}
-
-export const updateItem = async(req, res)=>{
+  export const updateItem = async (req, res) => {
     const item = await Item.findOne({
-        where:{
-            id : req.params.id
-        }
+      where: {
+        id: req.params.id
+      }
     });
-    if(!item) return res.status(404).json({msg: "No Data Found"});
-    let fileName = "";
-    if (req.files === null){
+  
+    if (!item) return res.status(404).json({ msg: "No Data Found" });
+     let fileName = "";
+     let imageUrl = "";
+  
+    // Cek apakah ada file yang diunggah
+    if (req.files === null) {
         fileName = item.image;
+        imageUrl = item.url;
+    
     } else {
-        const file = req.files.file;
-        const fileSize = file.data.length;
-        const ext = path.extname(file.name);
-        fileName = file.md5 + ext;
-        const allowedType = ['.png', '.jpg', '.jpeg'];
-
-        if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: 
-            "Invalid Images"});
-        if(fileSize > 5000000) return res.status(422).json({msg: "Image must be less than 5 MB"});
-
-        const filepath = `./public/images/${item.image}`;
-        fs.unlinkSync(filepath);
-
-        file.mv(`./public/images/${fileName}`, async(err)=>{
-            if(err) return res.status(500).json({msg: err.message});
-            
-        })
+      const file = req.files.file;
+      const fileSize = file.data.length;
+      const ext = path.extname(file.name);
+      fileName = file.md5 + ext;
+      const allowedType = ['.png', '.jpg', '.jpeg'];
+  
+      // Validasi tipe file dan ukuran file
+      if (!allowedType.includes(ext.toLowerCase())) {
+        return res.status(422).json({ msg: "Invalid Images" });
+      }
+  
+      if (fileSize > 5000000) {
+        return res.status(422).json({ msg: "Image must be less than 5 MB" });
+      }
+  
+      // Hapus file gambar yang lama di Firebase Cloud Storage
+      const oldImageRef = ref(storage, `images/${item.image}`);
+      await deleteObject(oldImageRef);
+  
+      // Upload gambar baru ke Firebase Cloud Storage
+      const newImageRef = ref(storage, `images/${fileName}`);
+      await uploadBytes(newImageRef, file.data);
+  
+      // Dapatkan URL unduhan baru dari Firebase Cloud Storage
+      imageUrl = await getDownloadURL(newImageRef);
     }
     const name = req.body.title;
-    const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
     const description = req.body.description;
     const price = req.body.price;
-
+    
     try {
-        await Item.update({name: name, image: fileName, url: url, description: description, price: price},{
-            where:{
-                id : req.params.id
-            }
-        });
-        res.status(200).json({msg: "Item Uploaded Successfuly"});
-    } catch (error) {
-        console.log(error.message);
-    }
-}
-
-export const deleteItem = async(req, res)=>{
-    const item = await Item.findOne({
-        where:{
-            id : req.params.id
+      // Update data item di database
+      await Item.update(
+        {
+          name: name,
+          image: fileName,
+          url: imageUrl,
+          description: description,
+          price: price
+        },
+        {
+          where: {
+            id: req.params.id
+          }
         }
-    });
-    if(!item) return res.status(404).json({msg: "No Data Found"});
-    try {
-        const filepath = `./public/images/${item.image}`;
-        fs.unlinkSync(filepath);
-        await Item.destroy({
-            where:{
-                id : req.params.id
-            }
-        });
-        res.status(200).json({msg: "Item Deleted Successfuly"});
+      );
+      res.status(200).json({ msg: "Item Updated Successfully" });
     } catch (error) {
-        console.log(error.message);
+      console.log(error.message);
+      return res.status(500).json({ msg: "Internal Server Error" });
     }
-}
+  };
+
+  export const deleteItem = async (req, res) => {
+    const item = await Item.findOne({
+      where: {
+        id: req.params.id
+      }
+    });
+  
+    if (!item) {
+      return res.status(404).json({ msg: "No Data Found" });
+    }
+  
+    try {
+      // Hapus file gambar dari Firebase Cloud Storage
+      const storage = getStorage(app);
+      const imageRef = ref(storage, `images/${item.image}`);
+     
+  
+      // Hapus item dari database
+      await Item.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+      await deleteObject(imageRef);
+      res.status(200).json({ msg: "Item Deleted Successfully" });
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json({ msg: "Internal Server Error" });
+    }
+  };
