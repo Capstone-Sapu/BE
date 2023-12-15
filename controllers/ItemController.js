b30bd8a1324de446a3eb8e630fafb571dde70e54
DelphiaAryana <delphiaaryana25@gmail.com>
2023-12-15 12:58:25 +0800

    Menghapus cloudinary

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -1,24 +1,26 @@
-import fs from "fs";
-import Item from "../models/ItemModel.js";
-import path from "path";
-import { Op } from "sequelize";
-import { initializeApp } from "firebase/app";
+/* eslint-disable no-shadow */
+/* eslint-disable consistent-return */
+/* eslint-disable import/extensions */
+import path from 'path';
+import { Op } from 'sequelize';
+import { initializeApp } from 'firebase/app';
 import {
   getStorage,
   ref,
   uploadBytes,
   getDownloadURL,
   deleteObject,
-} from "firebase/storage";
+} from 'firebase/storage';
+import Item from '../models/ItemModel.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsep7MLviWXB7C-L4zt6XI5kEgzoNrQmY",
-  authDomain: "sapu-project-af7ea.firebaseapp.com",
-  projectId: "sapu-project-af7ea",
-  storageBucket: "sapu-project-af7ea.appspot.com",
-  messagingSenderId: "159292158231",
-  appId: "1:159292158231:web:f521459b29dbba0e184752",
-  measurementId: "G-SBSGVKV41Y",
+  apiKey: 'AIzaSyCsep7MLviWXB7C-L4zt6XI5kEgzoNrQmY',
+  authDomain: 'sapu-project-af7ea.firebaseapp.com',
+  projectId: 'sapu-project-af7ea',
+  storageBucket: 'sapu-project-af7ea.appspot.com',
+  messagingSenderId: '159292158231',
+  appId: '1:159292158231:web:f521459b29dbba0e184752',
+  measurementId: 'G-SBSGVKV41Y',
 };
 
 const app = initializeApp(firebaseConfig);
@@ -42,7 +44,7 @@ export const getItems = async (req, res) => {
     res.json(items);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ msg: "Terjadi kesalahan server" });
+    res.status(500).json({ msg: 'Terjadi kesalahan server' });
   }
 };
 
@@ -60,10 +62,9 @@ export const getItemById = async (req, res) => {
 };
 
 export const saveItem = async (req, res) => {
-  if (req.files === null)
-    return res.status(400).json({ msg: "No file uploaded" });
+  if (req.files === null) { return res.status(400).json({ msg: 'No file uploaded' }); }
 
-  const file = req.files.file;
+  const { file } = req.files;
   const ext = path.extname(file.name);
   const fileName = file.md5 + ext;
   const storageRef = ref(storage, `images/${fileName}`);
@@ -74,8 +75,8 @@ export const saveItem = async (req, res) => {
     const imageUrl = await getDownloadURL(storageRef);
 
     const name = req.body.title;
-    const description = req.body.description;
-    const price = req.body.price;
+    const { description } = req.body;
+    const { price } = req.body;
 
     await Item.create({
       name,
@@ -85,9 +86,9 @@ export const saveItem = async (req, res) => {
       price,
     });
 
-    res.status(201).json({ msg: "Item created successfully" });
+    res.status(201).json({ msg: 'Item created successfully' });
   } catch (error) {
-    console.error("Error uploading file to Firebase Cloud Storage:", error);
+    console.error('Error uploading file to Firebase Cloud Storage:', error);
     return res.status(500).json({ msg: error.message });
   }
 };
@@ -99,65 +100,59 @@ export const updateItem = async (req, res) => {
     },
   });
 
-  if (!item) return res.status(404).json({ msg: "No Data Found" });
-  let fileName = "";
-  let imageUrl = "";
+  if (!item) return res.status(404).json({ msg: 'No Data Found' });
+  let fileName = '';
+  let imageUrl = '';
 
-  // Cek apakah ada file yang diunggah
   if (req.files === null) {
     fileName = item.image;
     imageUrl = item.url;
   } else {
-    const file = req.files.file;
+    const { file } = req.files;
     const fileSize = file.data.length;
     const ext = path.extname(file.name);
     fileName = file.md5 + ext;
-    const allowedType = [".png", ".jpg", ".jpeg"];
+    const allowedType = ['.png', '.jpg', '.jpeg'];
 
-    // Validasi tipe file dan ukuran file
     if (!allowedType.includes(ext.toLowerCase())) {
-      return res.status(422).json({ msg: "Invalid Images" });
+      return res.status(422).json({ msg: 'Invalid Images' });
     }
 
     if (fileSize > 5000000) {
-      return res.status(422).json({ msg: "Image must be less than 5 MB" });
+      return res.status(422).json({ msg: 'Image must be less than 5 MB' });
     }
 
-    // Hapus file gambar yang lama di Firebase Cloud Storage
     const oldImageRef = ref(storage, `images/${item.image}`);
     await deleteObject(oldImageRef);
 
-    // Upload gambar baru ke Firebase Cloud Storage
     const newImageRef = ref(storage, `images/${fileName}`);
     await uploadBytes(newImageRef, file.data);
 
-    // Dapatkan URL unduhan baru dari Firebase Cloud Storage
     imageUrl = await getDownloadURL(newImageRef);
   }
   const name = req.body.title;
-  const description = req.body.description;
-  const price = req.body.price;
+  const { description } = req.body;
+  const { price } = req.body;
 
   try {
-    // Update data item di database
     await Item.update(
       {
-        name: name,
+        name,
         image: fileName,
         url: imageUrl,
-        description: description,
-        price: price,
+        description,
+        price,
       },
       {
         where: {
           id: req.params.id,
         },
-      }
+      },
     );
-    res.status(200).json({ msg: "Item Updated Successfully" });
+    res.status(200).json({ msg: 'Item Updated Successfully' });
   } catch (error) {
     console.log(error.message);
-    return res.status(500).json({ msg: "Internal Server Error" });
+    return res.status(500).json({ msg: 'Internal Server Error' });
   }
 };
 
@@ -169,24 +164,22 @@ export const deleteItem = async (req, res) => {
   });
 
   if (!item) {
-    return res.status(404).json({ msg: "No Data Found" });
+    return res.status(404).json({ msg: 'No Data Found' });
   }
 
   try {
-    // Hapus file gambar dari Firebase Cloud Storage
     const storage = getStorage(app);
     const imageRef = ref(storage, `images/${item.image}`);
 
-    // Hapus item dari database
     await Item.destroy({
       where: {
         id: req.params.id,
       },
     });
     await deleteObject(imageRef);
-    res.status(200).json({ msg: "Item Deleted Successfully" });
+    res.status(200).json({ msg: 'Item Deleted Successfully' });
   } catch (error) {
     console.log(error.message);
-    return res.status(500).json({ msg: "Internal Server Error" });
+    return res.status(500).json({ msg: 'Internal Server Error' });
   }
 };
--- routes/ItemRoute.js
+++ routes/ItemRoute.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/named */
 /* eslint-disable import/extensions */
 /* eslint-disable indent */
 import express from 'express';
